636f9cfb648f381ef09e9b301b499c945f0e8640
94camilaaa <camilamarquescosta94@yahoo.com>
2020-06-14 21:58:35 +0000

    pronto

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://musicas-backend.herokuapp.com/musicas'
+    baseURL: 'https://musicas-backend.herokuapp.com'
 });
 
 export default api; 
